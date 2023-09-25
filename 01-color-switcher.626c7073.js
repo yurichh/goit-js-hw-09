cee27851a2c94a2f9741dd1449c09571de83e1eb
yurichh <132999504+yurichh@users.noreply.github.com>
2023-09-25 13:26:11 +0000

    Deploying to gh-pages from @ yurichh/goit-js-hw-09@b9e25970af99bfd526b22e50aaffc1fab78d080f 🚀

---
+++ 01-color-switcher.626c7073.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=(document.querySelector("button[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){setInterval((function(){o.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.626c7073.js.map
